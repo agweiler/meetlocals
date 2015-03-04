be3b22bb5cf4fcf531ad6059b226c34182864f1d
hewrin <hewrin@hotmail.com>
2015-03-04 16:42:41 +0800

    added a few mailer methods

--- app/mailers/hostmailer.rb
+++ app/mailers/hostmailer.rb
@@ -1,2 +1,13 @@
 class Hostmailer < ApplicationMailer
+
+  def receive_booking_request(host)
+    @host = host
+
+    mail(to: @host.email, subject: "Hurray! You have received a booking request")
+  end
+
+  def payment_completion(host)
+    @host = host
+    mail(to: @host.email, subject: "Payment has been completed!!!")
+  end
 end
