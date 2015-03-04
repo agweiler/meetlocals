c5b1ef9a93a6eb361f1007b17534c7c43c20040e
hewrin <hewrin@hotmail.com>
2015-03-04 14:49:49 +0800

    added actual email sending feature

--- app/controllers/guests/registrations_controller.rb
+++ app/controllers/guests/registrations_controller.rb
@@ -1,11 +1,5 @@
-# class Guests::RegistrationsController < Devise::RegistrationsController
-#  	protected
+class Guests::RegistrationsController < Devise::RegistrationsController
+ 	protected
 
-#  	def after_sign_up_path_for(resource)
-#     	edit_guest_profile_path(resource)
-#  	end
-
-#  	def after_sign_in_path_for(resource)
-# 		edit_guest_profile_path(resource)
-# 	end
-# end
+ 
+end
