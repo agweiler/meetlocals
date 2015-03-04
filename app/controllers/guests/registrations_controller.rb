abf12a68578b2b8b32e056c58797861c4fa3d989
hewrin <hewrin@hotmail.com>
2015-03-04 14:11:35 +0800

    Changed registration_controllers

--- app/controllers/guests/registrations_controller.rb
+++ app/controllers/guests/registrations_controller.rb
@@ -1,11 +1,11 @@
-class Guests::RegistrationsController < Devise::RegistrationsController
- 	protected
+# class Guests::RegistrationsController < Devise::RegistrationsController
+#  	protected
 
- 	def after_sign_up_path_for(resource)
-    	edit_guest_profile_path(resource)
- 	end
+#  	def after_sign_up_path_for(resource)
+#     	edit_guest_profile_path(resource)
+#  	end
 
- 	def after_sign_in_path_for(resource)
-		edit_guest_profile_path(resource)
-	end
-end
+#  	def after_sign_in_path_for(resource)
+# 		edit_guest_profile_path(resource)
+# 	end
+# end
