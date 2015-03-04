4fe90545d7e852c962acca907e535e9289c3c321
Adam Johari <adamjohari4@gmail.com>
2015-03-04 09:45:01 +0800

    eh

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,11 @@ host = Host.create(
 	username:"Host User",
 	email:"user@mail.com",
 	password:"password",
-	about:"I am a default Host!"
 	)
-host.experience.create(
+Host.last.experiences.create(
 	title:"Ameristralian Brunch",
 	description:"Have an american/australian hybrid	breakfast/lunch with us in Ameristralia",
-	halal: true,
+	is_halal: true,
 	cuisine:"American and Australian",
 	max_group_size: 10,
 	host_style: "All Ameristralian Hospitality (Wash your own dishes you lil'- I mean, I'll wash the plates, no problem!)",
