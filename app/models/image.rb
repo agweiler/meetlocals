9a08b9f901f6f5d9ba39077b1008a87aced8b2fb
Andreas <aw@blackboxdev.co>
2015-04-06 10:32:50 +0800

    remove overlay and other styling

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,9 +2,11 @@ class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
 
   has_attached_file :image_file,
-  styles: { :full=>"1232x466!", :large=> "600x600>",
-  :medium => "300x300>", :thumb => "100x100>" } #you can customise the storage path here using :path
+  styles: { :full=>"1232x466!", :large=> "600x600>", :medium => "300x300>", :thumb => "100x100>" } #you can customise the storage path here using :path
   # validates_attachment_content_type :image_file, content_type: /\Aimage\/.*\Z/
+  # need to define these according to the size they will be displayed on site. 
+  # host/avatar sizes ok
+  # experience show top images should be longer... aspect ratio will be 4:5 maybe?
 
   validates_attachment :image_file, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end	
