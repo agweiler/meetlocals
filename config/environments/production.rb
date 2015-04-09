8e20b8338c10e716965a27a25f29a95bae153a06
hewrin <hewrin@hotmail.com>
2015-04-09 14:52:23 +0800

    fixed weird rocket bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,13 +93,13 @@ Rails.application.configure do
    }
 
    config.paperclip_defaults = {
-          :storage =&gt; :s3,
-          :s3_credentials =&gt; {
-            :bucket =&gt; ENV['AWS_BUCKET'],
-            :access_key_id =&gt; ENV['AWS_ACCESS_KEY_ID'],
-            :secret_access_key =&gt; ENV['AWS_SECRET_ACCESS_KEY']
+          :storage => :s3,
+          :s3_credentials => {
+            :bucket => ENV['AWS_BUCKET'],
+            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
           },
-          :path =&gt; ":class/:id/:basename_:style.:extension",
-          :url =&gt; ":s3_sg_url"
+          :path => ":class/:id/:basename_:style.:extension",
+          :url => ":s3_sg_url"
       }
 end
