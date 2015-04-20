675995a00f915b8e67225999dd826f92ca4d60ad
Willie Loh <willie.loh@live.com>
2015-04-20 14:57:21 +0800

    error routing to public/404 & 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
   include SessionHelper
-  
+
   protected
 
 	def configure_permitted_parameters
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   #    	  edit_host_profile_path(resource)
   #    	else
   #    	  super
-  #    	end 
+  #    	end
   #  end
 
  def after_sign_in_path_for(resource_or_scope)
@@ -29,16 +29,33 @@ class ApplicationController < ActionController::Base
       (store_location.nil?) ? "/" : store_location.to_s
 
     when :host, Host
-      store_location = session[:forwarding_url]  
+      store_location = session[:forwarding_url]
       session.delete(:forwarding_url)
       (store_location.nil?) ? "/" : store_location.to_s
     else
       super
     end
-  end   
+  end
 
 
  	# alias_method :after_sign_in_path_for, :after_sign_up_path_for
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActionController::RoutingError, with: :unknown_error
+  private
+
+    def record_not_found
+      # render plain: "404 Not Found", status: 404
+      respond_to do |format|
+        format.html { redirect_to error_404_path, notice: '404'}
+      end
+    end
+
+    def unknown
+      respond_to do |format|
+        format.html { redirect_to error_500_path, notice: '500'}
+      end
+    end
 end
