9eed704695fe4a8baf32482eeea86064cad3355b
Willie Loh <willie.loh@live.com>
2015-04-22 11:54:59 +0800

    nav: refactoring _header to render only '_header_default'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def current_user
+    if current_host
+      current_host
+    elsif current_guest
+      current_guest
+    elsif
+      current_admin
+    end
+  end
+
+  def destroy_user_session_path
+    if current_guest
+      destroy_guest_session_path
+    elsif current_host
+      destroy_host_session_path
+    elsif current_admin
+      destroy_admin_session_path
+    end
+  end
 end
