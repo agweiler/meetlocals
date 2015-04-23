fa4bd7d24749c0ddf1ee7d545d7fa59ffb9009b8
hewrin <hewrin@hotmail.com>
2015-04-23 16:41:49 +0800

    fixed aws credentials, added page specific headers and event tracking testing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require moment
 //= require bootstrap-datetimepicker
 //= require jquery.raty.js
-
+//= require jquery-readyselector
 //= require bootstrap-wysihtml5
 //= require bootstrap-wysihtml5/wysihtml5x-toolbar.js
 //= require bootstrap-wysihtml5/handlebars.runtime.min.js
