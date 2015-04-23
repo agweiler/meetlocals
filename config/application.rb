75ba2fed4eae67597a780a2aa3e364635176ef96
hewrin <hewrin@hotmail.com>
2015-04-23 18:02:43 +0800

    changed config yaml file

--- config/application.rb
+++ config/application.rb
@@ -27,12 +27,13 @@ module Nasi
 
     config.active_job.queue_adapter = :sidekiq
 
-
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'aws_config.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
+    if Rails.env.development?
+      config.before_configuration do
+        env_file = File.join(Rails.root, 'config', 'aws_config.yml')
+        YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
-      end if File.exists?(env_file)
+        end if File.exists?(env_file)
+      end
     end
 
 
