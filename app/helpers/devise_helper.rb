0356c02888cf67e60cd0f3ff0c79a19dee14d321
Willie Loh <willie.loh@live.com>
2015-04-30 11:58:44 +0800

    enable mandatory image upload upon host/exp creation

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -14,13 +14,12 @@ module DeviseHelper
                       count: resource.errors.count,
                       resource: resource.class.model_name.human.downcase)
 
-    html = <<-HTML
-    <div id="error_explanation">
-      <h2>#{sentence}</h2>
-      <ul>#{messages}</ul>
-    </div>
-    HTML
-
+    # html = <<-HTML
+    # <div id="error_explanation">
+    #   <h2>#{sentence}</h2>
+    #   <ul>#{messages}</ul>
+    # </div>
+    # HTML
     # html.html_safe
     messages
   end
