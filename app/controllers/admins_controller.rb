661e82219cfcc52116d22379aaf410860a5184ef
hewrin <hewrin@hotmail.com>
2015-05-07 15:43:39 +0800

    added fixed price for exp and only admins are able to change price

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -8,4 +8,14 @@ class AdminsController < ApplicationController
 	def analytics
 	end
 
+	def settings
+		@prices = Price.all
+	end
+
+	def changeprice
+		price = Price.find params[:format]
+		price.update(price: params[:price][:price])
+		Experience.where(meal: price.meal).update_all(price: params[:price][:price])
+		redirect_to "/"
+	end
 end
