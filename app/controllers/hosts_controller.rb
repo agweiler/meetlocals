a64d81a8eaab47fcdda65c54e756079d68aa99bd
Willie Loh <willie.loh@live.com>
2015-05-08 14:32:59 +0800

    rename 'DOB' attr to 'dob'

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -27,7 +27,7 @@ class HostsController < ApplicationController
     # respond_to do |format|
     #   if @host.save
     #     format.html { redirect_to host, notice: 'host was successfully created.' }
-        
+
     #     # Create image after parent-host is saved
     #     new_img = @host.images.new
     #     new_img.image_file = @image_file
@@ -44,7 +44,7 @@ class HostsController < ApplicationController
   def update
     @image_file = params[:host].delete(:image_file)
     @host.update(host_params.except(:image_file))
-    if @image_file.present?  
+    if @image_file.present?
       if @host.images.present?
         @host.images.delete_all
       end
@@ -68,7 +68,7 @@ class HostsController < ApplicationController
   end
 
   def edit_host_profile # Edit profile page
-  	if current_host == nil 
+  	if current_host == nil
   	  deny_access_host
     else
       @host = Host.find(params[:id])
@@ -77,7 +77,7 @@ class HostsController < ApplicationController
 
   def update_host_profile # Create and Edit host profile
     @image_file = params[:host].delete(:image_file)
-    if @image_file.present? 
+    if @image_file.present?
       if @host.images.present?
         @host.images.delete_all
       end
@@ -86,7 +86,7 @@ class HostsController < ApplicationController
     puts "preparing for host update"
     if @host.update(host_params.except(:image_file))
       puts "updating host"
-      respond_to do |format| 
+      respond_to do |format|
         format.html { redirect_to edit_host_profile, notice: 'host profile was successfully updated.' }
       end
     end
@@ -100,6 +100,6 @@ class HostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def host_params
-      params.require(:host).permit(:username, :email, :password, :password_confirmation, :country, :state, :image_file, :suburb, :latitude, :longitude, :title, :first_name, :last_name, :languages, :street_adress, :intro, :neighbourhood, :additional_info, :DOB, :video_url )
+      params.require(:host).permit(:username, :email, :password, :password_confirmation, :country, :state, :image_file, :suburb, :latitude, :longitude, :title, :first_name, :last_name, :languages, :street_adress, :intro, :neighbourhood, :additional_info, :dob, :video_url )
     end
 end
