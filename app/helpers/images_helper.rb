23ce75c31859b0bc59d9f12b50161565bb10b35e
hewrin <hewrin@hotmail.com>
2015-07-29 08:27:48 +0800

    fixed breakage if hosts/guests/exps have no images

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,19 +1,23 @@
 module ImagesHelper
 	def check_images_host(id)
 		host = Host.find(id)
-		if host.images.first.image_file_file_name == ""
-			false
-		else
-			true
+		if host.images.present?
+			if host.images.first.image_file_file_name == ""
+				false
+			else
+				true
+			end
 		end
 	end
 
 	def check_images_guest(id)
 		guest = Guest.find(id)
-		if guest.images.first.image_file_file_name == ""
-			false
-		else
-			true
+		if guest.images.present?
+			if guest.images.first.image_file_file_name == ""
+				false
+			else
+				true
+			end
 		end
 	end
 end
