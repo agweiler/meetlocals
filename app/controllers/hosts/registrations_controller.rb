f0f4a7f6c8f2e07a0364c18a9b34d02cf6466f57
hewrin <hewrin@hotmail.com>
2015-09-21 15:18:26 +0800

    removed byebug

--- app/controllers/hosts/registrations_controller.rb
+++ app/controllers/hosts/registrations_controller.rb
@@ -1,16 +1,15 @@
 class Hosts::RegistrationsController < Devise::RegistrationsController
 
   def destroy
-    byebug
-    # resource.experiences.each do |exp|
-    #     exp.bookings.delete_all
-    # end
-    # resource.experiences.delete_all
-    # resource.delete
-    # Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-    # set_flash_message :notice, :destroyed if is_flashing_format?
-    # yield resource if block_given?
-    # respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+    resource.experiences.each do |exp|
+        exp.bookings.delete_all
+    end
+    resource.experiences.delete_all
+    resource.delete
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message :notice, :destroyed if is_flashing_format?
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
   end
 
   protected
