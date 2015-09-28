e36adcbbe938098c70eaf9f0445762147a4ae98f
hewrin <hewrin@hotmail.com>
2015-09-29 00:32:38 +0800

    changed content of reports

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,14 +1,20 @@
 module AdminsHelper
 
 	def convert_to_csv(bookings)
-		attributes = %w{event_date guest_name host_name booking_ID host_account_number price_paid host_paid?}
+		attributes = %w{event_date booking_ID guest_name guest_id host_name host_id host_address host_zip_and_town host_email host_phone host_account_number price_paid host_paid?}
 		row = []
 		bookings.each do |book|
 			one_booking = []
 			one_booking << book.date
-			one_booking << "#{book.guest.first_name} #{book.guest.last_name}"
-			one_booking << "#{book.experience.host.first_name} #{book.experience.host.last_name}"
 			one_booking << book.id
+			one_booking << "#{book.guest.first_name} #{book.guest.last_name}"
+			one_booking << book.guest.id
+			one_booking << "#{book.experience.host.first_name} #{book.experience.host.last_name}"
+			one_booking << book.experience.host.id
+			one_booking << book.experience.host.street_address
+			one_booking << book.experience.host.suburb
+			one_booking << book.experience.host.email
+			one_booking << book.experience.host.phone
 			one_booking << book.experience.host.bank_number
 			one_booking << book.experience.price
 			one_booking << book.host_paid
