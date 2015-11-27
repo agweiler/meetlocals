b0f42c7cfac3afe17f93b43724aed92f17c39b5f
hewrin <hewrin@hotmail.com>
2015-11-27 10:20:25 +0800

    changed static text content data type from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007083632) do
+ActiveRecord::Schema.define(version: 20151127021830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -332,7 +332,7 @@ ActiveRecord::Schema.define(version: 20151007083632) do
 
   create_table "static_texts", force: :cascade do |t|
     t.string   "name"
-    t.string   "content"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
