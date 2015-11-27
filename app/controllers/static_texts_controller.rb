c1683554dc1b7efe81d43a31ae92f8849d400abd
hewrin <hewrin@hotmail.com>
2015-11-27 11:24:12 +0800

    added video_url to static text sites

--- app/controllers/static_texts_controller.rb
+++ app/controllers/static_texts_controller.rb
@@ -1,19 +1,24 @@
 class StaticTextsController < ApplicationController
 	before_action :set_static_texts, only: [:edit, :update]
 	def index
-		@static_text = StaticText.all
+		@static_text = StaticText.all.order(:id)
 	end
 
 	def edit
 	end
 
 	def update
-		@static_text.update(content: params[:static_text][:content])
+		params[:static_text][:video_url].gsub!(/watch\?v=/,"embed/") if params[:static_text][:video_url] != nil
+		@static_text.update(static_text_params)
 		redirect_to static_texts_path
 	end
 
 	private
 		def set_static_texts
 			@static_text = StaticText.find(params[:id])
+		end
+
+		def static_text_params
+			params.require(:static_text).permit(:content, :video_url)
 		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127021830) do
+ActiveRecord::Schema.define(version: 20151127030125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -335,6 +335,7 @@ ActiveRecord::Schema.define(version: 20151127021830) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "video_url"
   end
 
   create_table "testimonials", force: :cascade do |t|
