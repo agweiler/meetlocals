892e96ec60ec1074b4900b13db82f33f3d3abe3c
hewrin <hewrin@hotmail.com>
2015-12-11 15:30:15 +0800

    changed env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
     config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'gentle-inlet-1053.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV['APP_HOST'] }
 
   config.action_mailer.delivery_method = :smtp
 
