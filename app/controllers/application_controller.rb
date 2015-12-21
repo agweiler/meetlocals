011bb6733a460df84e183ac9c63465cca7204f7d
hewrin <hewrin@hotmail.com>
2015-12-21 19:32:11 +0800

    added www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 
   def redirect_if_old
     if ENV['APP_HOST'] == "https://meet-the-danes.herokuapp.com" && request.host != "www.meetthedanes.com"
-      redirect_to "#{request.protocol}meetthedanes.com#{request.fullpath}", :status => :moved_permanently  
+      redirect_to "#{request.protocol}www.meetthedanes.com#{request.fullpath}", :status => :moved_permanently  
     end
   end
 
