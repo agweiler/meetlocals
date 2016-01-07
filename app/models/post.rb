58267762bfdb1067f2b745e1b0d5d5d8bd1d2154
hewrin <hewrin@hotmail.com>
2016-01-07 12:18:17 +0800

    added 'a' for sanitizer whitelist

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,6 @@ class Post < ActiveRecord::Base
 	private
 
 	def sanitize
-		self.body = Sanitize.fragment(self.body, :elements => ['br','b','i','strong','u','small'])
+		self.body = Sanitize.fragment(self.body, :elements => ['br','b','i','strong','u','small','a'])
 	end
 end
