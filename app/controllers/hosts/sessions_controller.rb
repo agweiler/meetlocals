b4fab0c64dd8b61d11b4fc030311c41b56b55eb1
Faris <hewrin@hotmail.com>
2016-04-18 21:04:30 +0800

    fixed routing

--- app/controllers/hosts/sessions_controller.rb
+++ app/controllers/hosts/sessions_controller.rb
@@ -2,11 +2,11 @@ class Hosts::SessionsController < Devise::SessionsController
 
   def create
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
+    flash[:notice] = "You have successfully logged in" 
     sign_in(resource_name, resource)
     yield resource if block_given?
     if resource.approved == false
-      respond_with resource, location: host_edit_path(resource)
+      respond_with resource, location: edit_host_path(resource)
     else
       respond_with resource, location: after_sign_in_path_for(resource)
     end
