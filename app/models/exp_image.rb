75a58c538fe8465a509ada7e50ee4153ca3f397d
Faris <hewrin@hotmail.com>
2016-06-02 00:32:56 +0800

    fix host form image

--- app/models/exp_image.rb
+++ app/models/exp_image.rb
@@ -19,36 +19,36 @@ class ExpImage < ActiveRecord::Base
           					:path => ":class/:id/:basename_:style.:extension",
           					:url => ":s3_sg_url"
 
- validates_attachment :image_file, content_type: { content_type: ["image/jpeg", "image/gif", "image/png", ".png"] }
- after_commit :queue_upload_to_s3, on: [:create, :update], unless: :skip_callback
- process_in_background :image_file, processing_image_url: 'missing_image.jpeg'
+  validates_attachment :image_file, content_type: { content_type: ["image/jpeg", "image/gif", "image/png", ".png"] }
+  #after_commit :queue_upload_to_s3, on: [:create, :update], unless: :skip_callback
+  process_in_background :image_file, processing_image_url: 'missing_image.jpeg'
 
- def queue_upload_to_s3
- 	if local_image_file_name == nil
- 	  temp_file_key.gsub! /\s+/, '+'
- 	  self.image_file_remote_url = temp_file_key
- 	  @skip_callback = true
- 	  save
- 	end
- end
+  def queue_upload_to_s3
+ 	  if local_image_file_name == nil
+ 	    temp_file_key.gsub! /\s+/, '+'
+ 	    self.image_file_remote_url = temp_file_key
+ 	    @skip_callback = true
+ 	    save
+ 	  end
+  end
 
- def upload_to_s3
- 	#makes the image_file become the local_image file (paperclip method)
-   self.image_file = Paperclip.io_adapters.for(self.local_image)
-   @skip_callback = true
-   save
- end
+  def upload_to_s3
+ 	  #makes the image_file become the local_image file (paperclip method)
+    self.image_file = Paperclip.io_adapters.for(self.local_image)
+    @skip_callback = true
+    save
+  end
 
- def image_file_remote_url=(url_value)
-   self.image_file = URI.parse(url_value)
-   # Assuming url_value is http://example.com/photos/face.png
-   # image_file_file_name == "face.png"
-   # image_file_content_type == "image/png"
-   @image_file_remote_url = url_value
- end
+  def image_file_remote_url=(url_value)
+    self.image_file = URI.parse(url_value)
+    # Assuming url_value is http://example.com/photos/face.png
+    # image_file_file_name == "face.png"
+    # image_file_content_type == "image/png"
+    @image_file_remote_url = url_value
+  end
 
 
- def skip_callback
-   @skip_callback
- end
+  def skip_callback
+    @skip_callback
+  end
 end
